4753022502b06d3ef328a33d94f95a545f21622a
Henry AT <yiperu@gmail.com>
2013-09-29 19:35:06 -0500

    Inicializacion del proyecto

---
+++ UsoWebview_Segmented/UsoWebview_Segmented/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UsoWebview_Segmented
+//
+//  Created by Henry AT on 9/29/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UsoWebview_Segmented/UsoWebview_Segmented/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UsoWebview_Segmented
+//
+//  Created by Henry AT on 9/29/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
